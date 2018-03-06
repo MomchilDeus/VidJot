443d90f891cb17e940b38c35680729a72269b654
MomchilDeus <momchil.kolev@protonmail.com>
2018-03-06 23:17:23 +0200

    changed user password

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://kolev:123StrongPassword!!@ds259258.mlab.com:59258/vidjot-prod'}
+    module.exports = {mongoURI: 'mongodb://kolev:kolev@ds259258.mlab.com:59258/vidjot-prod'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
