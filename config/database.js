6f58ebd893a7623cd3204486fb9421ef53eee515
MomchilDeus <momchil.kolev@protonmail.com>
2018-05-05 05:27:36 +0300

    cleanup

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://kolev:kolev@ds259258.mlab.com:59258/vidjot-prod'}
-} else {
-    module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
-}
